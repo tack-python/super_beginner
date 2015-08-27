465c5d91c820b5363810163f873e3bfabaf39e01
tack-python <toyama.tadashi+github@gmail.com>
2015-08-27 22:05:06 +1000

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+print "いくつまで数えますか？"
+
+num = raw_input()
+
+num = int(num)
+
+print "それじゃ", num ,"まで数えるよ！ Enterキーでスタート！"
+raw_input()
+
+def fizzbuzz(i):
+    if i%3==0 and i%5==0:
+        return "Fizz Buzz"
+    elif i%3==0:
+        return "Fizz"
+    elif i%5==0:
+        return "Buzz"
+    else:
+        return str(i)
+
+for i in range(1, num+1):
+    print fizzbuzz(i) , "|" , 
+
+print "\n\nありがとうございました！"
